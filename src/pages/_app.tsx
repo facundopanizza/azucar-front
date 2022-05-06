1e5cc24016f1b29dc9b63bc8f67767b88899615d
Facundo Panizza <facundo@panizza.dev>
2022-05-06 20:50:38 -0300

    Added redirect on fail token

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,28 @@
 import '../styles/index.css';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import {onError } from "@apollo/client/link/error";
 import {  setContext} from '@apollo/client/link/context'
+import { Router } from 'next/router';
 
 const httpLink = createHttpLink({ 
   uri: `${process.env.NEXT_PUBLIC_BACK_URL}/graphql`
 })
+
+const errorControl = onError(({ networkError, graphQLErrors }) => {
+  if (graphQLErrors) {
+    for (let err of graphQLErrors) {
+      if (err.extensions.code === 'UNAUTHENTICATED' || err.extensions.message === 'Context creation failed: you must be logged in')   {
+        console.log('asdas');
+        localStorage.removeItem('token');
+        window.location.replace('/login');
+      }
+    }
+  }
+
+  if (networkError) {
+    console.log(" [Network error]:", networkError)
+  };
+});
 
 const authLink = setContext((_, { headers} ) => {
   const token = localStorage.getItem('token');
@@ -19,7 +37,7 @@ const authLink = setContext((_, { headers} ) => {
 
 const client = new ApolloClient({
   uri: `${process.env.NEXT_PUBLIC_BACK_URL}/graphql`,
-  link: authLink.concat(httpLink),
+  link: errorControl.concat(authLink.concat(httpLink)),
   cache: new InMemoryCache(),
 });
 
