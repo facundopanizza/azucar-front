69d93c4dc8da155a193b570a2444698cc50d1ab8
Facundo Panizza <facundo@panizza.dev>
2024-07-05 11:34:16 -0300

    Deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,25 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/client/link/error';
 import '../styles/index.css';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import {onError } from "@apollo/client/link/error";
-import {  setContext} from '@apollo/client/link/context'
-import { Router } from 'next/router';
 
-const httpLink = createHttpLink({ 
-  uri: `https://azucar-api.panizza.dev/graphql`
-})
+const httpLink = createHttpLink({
+  uri: `https://azucar-api.panizza.dev/graphql`,
+});
 
 const errorControl = onError(({ networkError, graphQLErrors }) => {
   if (graphQLErrors) {
     for (let err of graphQLErrors) {
-      if (err.extensions.code === 'UNAUTHENTICATED' || err.extensions.message === 'Context creation failed: you must be logged in')   {
+      if (
+        err.extensions.code === 'UNAUTHENTICATED' ||
+        err.extensions.message ===
+          'Context creation failed: you must be logged in'
+      ) {
         localStorage.removeItem('token');
         window.location.replace('/login');
       }
@@ -19,12 +27,12 @@ const errorControl = onError(({ networkError, graphQLErrors }) => {
   }
 
   if (networkError) {
-    console.log(" [Network error]:", networkError)
-  };
+    console.log(' [Network error]:', networkError);
+  }
 });
 
-const authLink = setContext((_, { headers} ) => {
-  let token = "";
+const authLink = setContext((_, { headers }) => {
+  let token = '';
 
   if (typeof window !== 'undefined') {
     token = localStorage.getItem('token');
@@ -34,9 +42,9 @@ const authLink = setContext((_, { headers} ) => {
     headers: {
       ...headers,
       authorization: token ? token : '',
-    }
-  }
-})
+    },
+  };
+});
 
 const client = new ApolloClient({
   uri: `https://azucar-api.panizza.dev/graphql`,
